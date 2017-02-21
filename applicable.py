9885522ced8a363a5abe34a471e82b1d45094356
shuwa <mana_burn_vo@yahoo.co.jp>
2017-02-21 11:25:01 +0900

    pycodestyle applicable.py

--- applicable.py
+++ applicable.py
@@ -5,7 +5,8 @@ class Applicable:
     requirement: {var:value}
     achievement: {var:value}
     '''
-    def __init__(self,name):
+
+    def __init__(self, name):
         self.name = name
 
     @classmethod
@@ -13,11 +14,11 @@ class Applicable:
         ap = cls(name)
         ap.prevail = prevail
         ap.effect = effect
-        ap.requirement = {} 
-        ap.achievement = {} 
-        for k,v in prevail.items():
+        ap.requirement = {}
+        ap.achievement = {}
+        for k, v in prevail.items():
             ap.requirement[k] = v
-        for var,(fr,to) in effect.items():
+        for var, (fr, to) in effect.items():
             ap.achievement[var] = to
             if fr == -1:
                 continue
@@ -28,22 +29,22 @@ class Applicable:
     def from_requirement(cls, name, requirement, achievement):
         ap = cls(name)
         ap.requirement = requirement
-        ap.achievement = achievement 
+        ap.achievement = achievement
         ap.prevail = {}
         ap.effect = {}
-        for k,v in ap.requirement.items():
+        for k, v in ap.requirement.items():
             if k in ap.achievement:
-                ap.effect[k] = (ap.requirement[k],ap.achievement[k])
+                ap.effect[k] = (ap.requirement[k], ap.achievement[k])
             else:
                 ap.prevail[k] = ap.requirement[k]
-        for k,v in ap.achievement.items():
-            if not k in ap.requirement:
-                ap.effect[k] = (-1,v)
+        for k, v in ap.achievement.items():
+            if k not in ap.requirement:
+                ap.effect[k] = (-1, v)
         return ap
 
-    def is_applicable(self,state):
-        for (var,value) in self.requirement.items():
-            if not var in state.assignment:
+    def is_applicable(self, state):
+        for (var, value) in self.requirement.items():
+            if var not in state.assignment:
                 continue
             if not state.assignment[var] == value:
                 return False
@@ -52,5 +53,5 @@ class Applicable:
     def __repr__(self):
         return self.name
 
-    def __lt__(self,other):
+    def __lt__(self, other):
         return self.name < other.name
